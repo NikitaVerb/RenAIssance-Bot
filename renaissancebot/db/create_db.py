2cf5db868480f6362f1378bb98be1f9778f8f585
Никита Кузнецов <nikkuzya21@gmail.com>
2024-08-09 15:37:26 +0300

    added command unlink_all

--- renaissancebot/db/create_db.py
+++ renaissancebot/db/create_db.py
@@ -28,8 +28,8 @@ async def create_db():
         # Создаем таблицу связей пользователей и почт
         await cursor.execute('''
             CREATE TABLE IF NOT EXISTS UserEmails (
-                user_id INTEGER,
-                email TEXT,
+                user_id INTEGER NOT NULL,
+                email TEXT NOT NULL,
                 FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE RESTRICT,
                 FOREIGN KEY (email) REFERENCES Emails (email) ON DELETE RESTRICT,
                 PRIMARY KEY (user_id, email)
--- renaissancebot/handlers/admin/unlink_from_account.py
+++ renaissancebot/handlers/admin/unlink_from_account.py
@@ -4,14 +4,15 @@ from aiogram import Router, types
 from aiogram.filters import Command, CommandObject
 
 import renaissancebot.filters.user_rights
-from db import check_account_in_db, check_user_email_in_db, unlink_user_from_account, get_user_id
+from db import check_account_in_db, check_user_email_in_db, unlink_user_from_account, get_user_id, \
+    unlink_all_users_from_account
 
 router = Router()
 router.message.filter(renaissancebot.filters.user_rights.UserIsAdmin())
 
 
 @router.message(Command('unlink'))
-async def unlink_from_account(message: types.Message, command: CommandObject):
+async def unlink_from_account_handler(message: types.Message, command: CommandObject):
     try:
         # Проверяем, что команда содержит два аргумента (email аккаунта и email пользователя)
         if not command.args or len(command.args.split()) != 2:
@@ -47,4 +48,32 @@ async def unlink_from_account(message: types.Message, command: CommandObject):
     except Exception as e:
         # Обработка исключений и уведомление пользователя о проблеме
         await message.answer("Произошла ошибка при отвязывании пользователя от аккаунта")
+        logging.exception(e)
+
+
+@router.message(Command('unlink_all'))
+async def unlink_all_users_from_account_handler(message: types.Message, command: CommandObject):
+    try:
+        # Проверяем, что команда содержит один аргумент (email аккаунта)
+        if not command.args or len(command.args.split()) != 1:
+            await message.answer("Неправильный формат команды. Используйте: /unlink_all <account_email>", parse_mode=None)
+            return
+
+        # Получаем email аккаунта
+        account_email = command.args.strip()
+
+        # Проверяем, что аккаунт существует в базе данных
+        if not await check_account_in_db(account_email):
+            await message.answer("Такого аккаунта нет в базе")
+            return
+
+        # Отвязываем всех пользователей от аккаунта
+        await unlink_all_users_from_account(account_email)
+
+        # Отправляем сообщение о успешном отвязывании всех пользователей
+        await message.answer(f"Все пользователи успешно отвязаны от аккаунта {account_email}")
+
+    except Exception as e:
+        # Обработка исключений и уведомление пользователя о проблеме
+        await message.answer("Произошла ошибка при отвязывании всех пользователей от аккаунта")
         logging.exception(e)
