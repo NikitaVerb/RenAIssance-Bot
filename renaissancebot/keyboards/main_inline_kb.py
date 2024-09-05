833fe7d192b73ed327373b48f260e6e3b00fbbd6
Никита Кузнецов <nikkuzya21@gmail.com>
2024-09-05 17:57:24 +0300

    modified main_inline_kb added tg-chanel

--- renaissancebot/keyboards/main_inline_kb.py
+++ renaissancebot/keyboards/main_inline_kb.py
@@ -7,9 +7,10 @@ def main_inline_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(text="🛒Каталог", callback_data="catalog")
     builder.button(text="👤Профиль", callback_data="profile")
-    builder.button(text="💌Отзывы", callback_data="reviews")
+    builder.button(text="💌Отзывы", url='https://t.me/RenAIssanceOpenFeedback')
     builder.button(text="🕊FAQ", callback_data="faq")
     builder.button(text="❓Тех. поддержка", url='https://t.me/RenAIssanceSupport')
+    builder.button(text="👋Тг-канал", url='https://t.me/plusgpt4')
     builder.adjust(2)
     kb = builder.as_markup()
     return kb
