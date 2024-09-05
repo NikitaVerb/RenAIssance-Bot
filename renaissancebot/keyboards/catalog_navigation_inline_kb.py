1a7a8baceae2a9c503c7a42e5613e9fb049626a5
Никита Кузнецов <nikkuzya21@gmail.com>
2024-09-05 16:17:53 +0300

    added back_to_menu button in catalog

--- renaissancebot/keyboards/catalog_navigation_inline_kb.py
+++ renaissancebot/keyboards/catalog_navigation_inline_kb.py
@@ -3,8 +3,6 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def catalog_navigation_inline_kb(total_page: int, page: int) -> InlineKeyboardMarkup:
-    builder = InlineKeyboardBuilder()
-
     match page:
         case 1:
             kb = [
@@ -12,23 +10,26 @@ def catalog_navigation_inline_kb(total_page: int, page: int) -> InlineKeyboardMa
                  InlineKeyboardButton(text="➡️", callback_data=f'navigation_{page + 1}')],
                 [InlineKeyboardButton(text="1 месяц", callback_data=f'subscribe_1'),
                  InlineKeyboardButton(text="3 месяца", callback_data=f'subscribe_3'),
-                 InlineKeyboardButton(text="6 месяца", callback_data=f'subscribe_6')]
+                 InlineKeyboardButton(text="6 месяца", callback_data=f'subscribe_6')],
+                [InlineKeyboardButton(text="↩Назад в меню", callback_data="menu")]
             ]
             kb = InlineKeyboardMarkup(inline_keyboard=kb)
 
         case 2:
-            builder.button(text="⬅️", callback_data=f'navigation_{page - 1}')
-            builder.button(text=f"{page}/{total_page}", callback_data="pass")
-            builder.button(text="Оставить заявку", url='https://t.me/RenAIssanceSupport')
-            builder.adjust(2)
-            kb = builder.as_markup()
+            kb = [
+                [InlineKeyboardButton(text="⬅️", callback_data=f'navigation_{page - 1}'),
+                 InlineKeyboardButton(text=f"{page}/{total_page}", callback_data="pass")],
+                [InlineKeyboardButton(text="Оставить заявку", url='https://t.me/RenAIssanceSupport')],
+                [InlineKeyboardButton(text="↩Назад в меню", callback_data="menu")]]
+
+            kb = InlineKeyboardMarkup(inline_keyboard=kb)
         case _:
+            kb = [
+                [InlineKeyboardButton(text="⬅️", callback_data=f'navigation_{page - 1}'),
+                 InlineKeyboardButton(text=f"{page}/{total_page}", callback_data="pass"),
+                 InlineKeyboardButton(text="➡️", callback_data=f'navigation_{page + 1}')],
+                [InlineKeyboardButton(text="Оставить заявку", url='https://t.me/RenAIssanceSupport')],
+                [InlineKeyboardButton(text="↩Назад в меню", callback_data="menu")]]
 
-            builder.button(text="⬅️", callback_data=f'navigation_{page - 1}')
-            builder.button(text=f"{page}/{total_page}", callback_data="pass")
-            builder.button(text="➡️", callback_data=f'navigation_{page + 1}')
-            builder.button(text="Oформить", callback_data=f'subscribe')
-            builder.adjust(3)
-            kb = builder.as_markup()
-
+            kb = InlineKeyboardMarkup(inline_keyboard=kb)
     return kb
--- renaissancebot/keyboards/main_inline_kb.py
+++ renaissancebot/keyboards/main_inline_kb.py
@@ -9,7 +9,7 @@ def main_inline_kb() -> InlineKeyboardMarkup:
     builder.button(text="👤Профиль", callback_data="profile")
     builder.button(text="💌Отзывы", callback_data="reviews")
     builder.button(text="🕊FAQ", callback_data="faq")
-    builder.button(text="❓Тех. поддержка", callback_data="support")
+    builder.button(text="❓Тех. поддержка", url='https://t.me/RenAIssanceSupport')
     builder.adjust(2)
     kb = builder.as_markup()
     return kb
