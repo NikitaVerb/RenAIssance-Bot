e96bf0ee09f3efc98c7258d8b81a06e55fc8ff8a
Никита Кузнецов <nikkuzya21@gmail.com>
2024-09-05 13:26:28 +0300

    modified catalog

--- renaissancebot/handlers/user/catalog.py
+++ renaissancebot/handlers/user/catalog.py
@@ -9,18 +9,17 @@ router = Router()
 
 @router.message(F.text == 'Каталог')
 async def catalog(message: Message):
-    photo_id = "AgACAgIAAxkBAAIDimazVhnKFOzxUYp5kKNqymkbpsUaAAK64DEb49-YSejBOi7eNYlWAQADAgADeAADNQQ"
-    await message.answer_photo(photo=photo_id, caption='<b>DECA - 649/месяц.</b>\n\nШеринговый аккаунт до 7 человек.'
-                                                       'Активно модерируется, чтобы всем хватало запросов. Комфорт '
-                                                       'и эффективность в одном тарифе. Самый частый выбор наших клиентов',
-                               parse_mode=ParseMode.HTML, reply_markup=catalog_navigation_inline_kb(2, 1))
+    await message.answer(
+        '<b>DECA - 649/месяц.</b>\n\nCовместный аккаунт до 7 человек с ChatGPT-4 (Plus).'
+        ' Активно модерируется, чтобы всем хватало запросов. Комфорт и эффективность в одном тарифе.',
+        parse_mode=ParseMode.HTML, reply_markup=catalog_navigation_inline_kb(2, 1))
 
 
 @router.callback_query(F.data.startswith("navigation_"))
 async def catalog_process(callback: CallbackQuery, bot: Bot):
     await callback.answer()
     page = int(callback.data.split("_")[1])
-    photo_id = "AgACAgIAAxkBAAIDimazVhnKFOzxUYp5kKNqymkbpsUaAAK64DEb49-YSejBOi7eNYlWAQADAgADeAADNQQ"
+
     match page:
         case 1:
             text = ("<b>DECA - 649/месяц.</b>\n\nШеринговый аккаунт до 7 человек."
@@ -28,13 +27,17 @@ async def catalog_process(callback: CallbackQuery, bot: Bot):
                     " Самый частый выбор наших клиентов.")
         case 2:
             text = ("<b>UNICO - 2500/месяц.</b>\n\nИндивидуальный аккаунт, который будет доступен только вам."
-                    " Вы получите полный доступ к функционалу ChatGPT-4. Оставье заявку, чтобы приобрести.")
+                    " Вы получите полный доступ к функционалу ChatGPT-4. Оставьте заявку, чтобы приобрести.")
         case _:
             text = 'Произошла ошибка'
-    await bot.edit_message_media(chat_id=callback.message.chat.id,
-                                 message_id=callback.message.message_id,
-                                 media=InputMediaPhoto(media=photo_id, caption=text),
-                                 reply_markup=catalog_navigation_inline_kb(2, page))
+
+    await bot.edit_message_text(
+        chat_id=callback.message.chat.id,
+        message_id=callback.message.message_id,
+        text=text,
+        parse_mode=ParseMode.HTML,
+        reply_markup=catalog_navigation_inline_kb(2, page)
+    )
 
 
 @router.callback_query(F.data == 'pass')
