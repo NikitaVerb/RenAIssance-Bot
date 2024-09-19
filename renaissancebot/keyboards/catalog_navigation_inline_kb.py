365f644b3b9f36b0597e1ecfbb613254851b3e73
Никита Кузнецов <nikkuzya21@gmail.com>
2024-09-19 10:09:43 +0300

    modified text in faq, profile and catalog_navigation

--- renaissancebot/keyboards/catalog_navigation_inline_kb.py
+++ renaissancebot/keyboards/catalog_navigation_inline_kb.py
@@ -6,7 +6,7 @@ def catalog_navigation_inline_kb(total_page: int, page: int) -> InlineKeyboardMa
         case 1:
             kb = [
                 [InlineKeyboardButton(text=f"{page}/{total_page} стр.", callback_data="pass"),
-                 InlineKeyboardButton(text="далее ➡️", callback_data=f'navigation_{page + 1}')],
+                 InlineKeyboardButton(text="Далее ➡️", callback_data=f'navigation_{page + 1}')],
                 [InlineKeyboardButton(text="Купить 1 месяц — 649 руб", callback_data=f'subscribe_1')],
                 [InlineKeyboardButton(text="Купить 3 месяца — 1749 руб", callback_data=f'subscribe_3')],
                 [InlineKeyboardButton(text="Купить 6 месяцев — 3349 руб", callback_data=f'subscribe_6')],
@@ -16,7 +16,7 @@ def catalog_navigation_inline_kb(total_page: int, page: int) -> InlineKeyboardMa
 
         case 2:
             kb = [
-                [InlineKeyboardButton(text="⬅️ назад", callback_data=f'navigation_{page - 1}'),
+                [InlineKeyboardButton(text="⬅️ Назад", callback_data=f'navigation_{page - 1}'),
                  InlineKeyboardButton(text=f"{page}/{total_page} стр.", callback_data="pass")],
                 [InlineKeyboardButton(text="Купить 1 месяц — 2500 руб", callback_data=f'subscribe_ind')],
                 [InlineKeyboardButton(text="↩ Назад в меню", callback_data="menu")]]
