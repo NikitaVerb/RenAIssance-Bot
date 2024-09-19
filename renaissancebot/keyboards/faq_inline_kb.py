07bd14b603b3865fba59632a906f648fd5ef21c6
Никита Кузнецов <nikkuzya21@gmail.com>
2024-09-19 10:24:13 +0300

    modified text in faq keyboard

--- renaissancebot/keyboards/faq_inline_kb.py
+++ renaissancebot/keyboards/faq_inline_kb.py
@@ -7,7 +7,7 @@ def faq_inline_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(text="Как это работает?", callback_data="faq_1")
     builder.button(text="Почему мы?", callback_data="faq_2")
-    builder.button(text="Какие ограничения у общего аккаунта?", callback_data="faq_3")
+    builder.button(text="Будет ли хватать запросов?", callback_data="faq_3")
     builder.button(text="На какой период действует подписка?", callback_data="faq_4")
     builder.button(text="Нужен ли VPN?", callback_data="faq_5")
     builder.button(text="↩ Назад в меню", callback_data="menu")
